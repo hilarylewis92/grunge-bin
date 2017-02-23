eb71b9ac3b1d74cfb38d81a5884a6906015e6ee7
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-23 11:39:51 -0700

    Add offender to app.locals and post to /api/offenders

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const md5 = require('md5')
 
 
 const port = process.env.PORT || 3000
 
-app.locals.title = 'Final Assessment'
-
+app.locals.title = 'Grunge Bin'
+app.locals.offenders = [
+  {
+    name: 'Donald Trump',
+    offense: 'He took my happiness forever',
+    forgiven: false,
+    date: Date.now()
+  }
+]
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '.', 'public/index.html'))
 })
+
+app.get('/api/offenders', (req, res) => {
+  const offenders = app.locals.offenders
+
+  res.json({ offenders })
+})
+
+app.post('/api/offenders', (req, res) => {
+  const offense = req.body
+  const id = md5(offense)
+  const offender = { id, offense }
+
+  app.locals.offenders.push(offender)
+})
+
 
 app.listen(port, () => {
   console.log(`${app.locals.title} is listening on port ${port}.`)
