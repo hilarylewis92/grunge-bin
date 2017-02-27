f61abfc3706f9de47db9d1ba841738adcb4b525f
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-27 14:24:50 -0700

    Add more people to mock data

--- test/units.spec.js
+++ test/units.spec.js
@@ -24,19 +24,41 @@ const offenders = [
       offense: 'being pretty',
     }
   },
+  {
+    id: '912ec803b2ce49e4a541068d495ab572',
+    offender: {
+      date: 1488226849430,
+      forgiven: false,
+      name: "Alex",
+      offense: 'being awesome',
+    }
+  },
+  {
+    id: '912ec803b2ce49e4a541068d495ab573',
+    offender: {
+      date: 1488226849431,
+      forgiven: false,
+      name: "Brittany",
+      offense: 'not living in Denver',
+    }
+  },
 ]
 
 describe('Sort by', () => {
   it('returns sorted by Date', () => {
     let subject = sortByDate(offenders)
-    expect(subject[0].offender.name).to.equal('Meeka')
-    expect(subject[1].offender.name).to.equal('Steve')
+    expect(subject[0].offender.name).to.equal('Brittany')
+    expect(subject[1].offender.name).to.equal('Alex')
+    expect(subject[2].offender.name).to.equal('Meeka')
+    expect(subject[3].offender.name).to.equal('Steve')
   })
 
   it('returns sorted by Name', () => {
     let subject = sortByName(offenders)
-    expect(subject[0].offender.name).to.equal('Meeka')
-    expect(subject[1].offender.name).to.equal('Steve')
+    expect(subject[0].offender.name).to.equal('Alex')
+    expect(subject[1].offender.name).to.equal('Brittany')
+    expect(subject[2].offender.name).to.equal('Meeka')
+    expect(subject[3].offender.name).to.equal('Steve')
   })
 })
 
@@ -44,13 +66,13 @@ describe('countOffenders', () => {
   it('measures total, unforgiven and forgiven', () => {
     let subject
     subject = countOffenders(offenders)
-    expect(subject.totalOffenders).to.equal(2)
-    expect(subject.totalUnforgiven).to.equal(2)
+    expect(subject.totalOffenders).to.equal(4)
+    expect(subject.totalUnforgiven).to.equal(4)
     expect(subject.totalForgiven).to.equal(0)
     offenders[1].offender.forgiven = true
     subject = countOffenders(offenders)
-    expect(subject.totalOffenders).to.equal(2)
-    expect(subject.totalUnforgiven).to.equal(1)
+    expect(subject.totalOffenders).to.equal(4)
+    expect(subject.totalUnforgiven).to.equal(3)
     expect(subject.totalForgiven).to.equal(1)
   })
 
