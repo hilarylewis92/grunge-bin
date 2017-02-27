54999e894bf2fb00ad83a84cae2b20ceedf1fe0d
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-27 14:20:47 -0700

    Refactor offenders array into global scope

--- test/units.spec.js
+++ test/units.spec.js
@@ -5,32 +5,32 @@ const countOffenders = require('../public/count.js').countOffenders
 const sortByDate = require('../public/sort.js').sortByDate
 const sortByName = require('../public/sort.js').sortByName
 
-describe('Sort by', () => {
-  const offenders = [
-    {
-      id: '912ec803b2ce49e4a541068d495ab570',
-      offender: {
-        date: 1488226849429,
-        forgiven: false,
-        name: "Steve",
-        offense: 'this assessment',
-      }
-    },
-    {
-      id: '912ec803b2ce49e4a541068d495ab571',
-      offender: {
-        date: 1488226849427,
-        forgiven: false,
-        name: "Meeka",
-        offense: 'being pretty',
-      }
-    },
-  ]
+const offenders = [
+  {
+    id: '912ec803b2ce49e4a541068d495ab570',
+    offender: {
+      date: 1488226849428,
+      forgiven: false,
+      name: "Steve",
+      offense: 'this assessment',
+    }
+  },
+  {
+    id: '912ec803b2ce49e4a541068d495ab571',
+    offender: {
+      date: 1488226849429,
+      forgiven: false,
+      name: "Meeka",
+      offense: 'being pretty',
+    }
+  },
+]
 
+describe('Sort by', () => {
   it('returns sorted by Date', () => {
     let subject = sortByDate(offenders)
-    expect(subject[0].offender.name).to.equal('Steve')
-    expect(subject[1].offender.name).to.equal('Meeka')
+    expect(subject[0].offender.name).to.equal('Meeka')
+    expect(subject[1].offender.name).to.equal('Steve')
   })
 
   it('returns sorted by Name', () => {
@@ -41,27 +41,6 @@ describe('Sort by', () => {
 })
 
 describe('countOffenders', () => {
-  const offenders = [
-    {
-      id: '912ec803b2ce49e4a541068d495ab570',
-      offender: {
-        date: 1488226849428,
-        forgiven: false,
-        name: "Steve",
-        offense: 'this assessment',
-      }
-    },
-    {
-      id: '912ec803b2ce49e4a541068d495ab571',
-      offender: {
-        date: 1488226849429,
-        forgiven: false,
-        name: "Meeka",
-        offense: 'being pretty',
-      }
-    },
-  ]
-
   it('measures total, unforgiven and forgiven', () => {
     let subject
     subject = countOffenders(offenders)
@@ -82,35 +61,3 @@ describe('countOffenders', () => {
     expect(subject.totalForgiven).to.equal(0)
   })
 })
-
-// const chai = require('chai')
-// const should = chai.should()
-// const expect = chai.expect()
-// const chaiHttp = require('chai-http')
-// const server = require('../server')
-// const jsdom = require('mocha-jsdom')
-//
-// chai.use(chaiHttp)
-//
-// describe('Unit test', () =>  {
-//   jsdom()
-//
-//   it('should display a title', (done) => {
-//     chai.request(server)
-//     .get('/')
-//     .end((e => rr, res) {
-//       res.text.should.include('Grunge Bin')
-//       done()
-//     })
-//   })
-//
-//   it('should display a list', (done) => {
-//     chai.request(server)
-//     .get('/api/offenders')
-//     .end((e => rr, res) {
-//       res.text.should.include('offenders')
-//       res.text.should.include('offender')
-//       done()
-//     })
-//   })
-// })
